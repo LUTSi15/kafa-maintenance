4ee95522d651a9b89cf16cf47943310489a331e4
rizalromzi01 <rizaldanial0601@gmail.com>
2024-06-10 02:34:01 +0800

    1

--- resources/views/ManageKAFAActivities/KAFAViewActivity.blade.php
+++ resources/views/ManageKAFAActivities/KAFAViewActivity.blade.php
@@ -3,60 +3,71 @@
     <div class="container mt-4">
         <div class="card">
             <div class="card-header bg-success text-white">
-                <h5 class="mb-0 text-dark">New Activity</h5>
+                <h5 class="mb-0">{{$activity->activityName}}</h5>
             </div>
             <div class="card-body">
                 <form>
                     <div class="row mb-3">
                         <label for="activityName" class="col-sm-2 col-form-label text-center">Activity Name</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="activityName" value="">
+                            <input type="text" readonly class="form-control" id="activityName"
+                                value="{{$activity->activityName}}">
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="venue" class="col-sm-2 col-form-label text-center">Venue</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="venue" value="">
+                            <input type="text" readonly class="form-control" id="venue" value="{{$activity->venue}}">
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="dateStart" class="col-sm-2 col-form-label text-center">Date</label>
                         <div class="col-sm-4">
-                            <input type="date" class="form-control" id="dateStart" value="">
+                            <input type="date" readonly class="form-control" id="dateStart" value="{{$activity->dateStart}}">
                             <label for="dateEnd" class="col-sm-12 col-form-label text-center">Until</label>
-                            <input type="date" class="form-control" id="dateEnd" value="">
+                            <input type="date" readonly class="form-control" id="dateEnd" value="{{$activity->dateEnd}}">
                         </div>
                         <label for="timeStart" class="col-sm-2 col-form-label text-center">Time</label>
                         <div class="col-sm-4">
-                            <input type="time" class="form-control" id="timeStart" value="">
+                            <input type="time" readonly class="form-control" id="timeStart" value="{{$activity->timeStart}}">
                             <label for="timeEnd" class="col-sm-12 col-form-label text-center">Until</label>
-                            <input type="time" class="form-control" id="timeEnd" value="">
+                            <input type="time" readonly class="form-control" id="timeEnd" value="{{$activity->timeEnd}}">
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="attendees" class="col-sm-2 col-form-label text-center">Attendees</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="attendees" value="">
+                            <input type="text" readonly class="form-control" id="attendees"
+                                value="{{$activity->attendees}}">
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="organizerName" class="col-sm-2 col-form-label text-center">People In Charge</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="organizerName" value="">
+                            <input type="text" readonly class="form-control" id="organizerName"
+                                value="{{$activity->organizerName}}">
                         </div>
                     </div>
                     <div class="row mb-3">
                         <label for="description" class="col-sm-2 col-form-label text-center">Description</label>
                         <div class="col-sm-10">
-                            <textarea class="form-control" id="description" rows="3"></textarea>
+                            <textarea readonly class="form-control" id="description" rows="3">{{$activity->description}}</textarea>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="feedback" class="col-sm-2 col-form-label text-center">Feedback</label>
+                        <div class="col-sm-10">
+                            <textarea readonly class="form-control" id="feedback" rows="3">{{$activity->feedback}}</textarea>
                         </div>
                     </div>
                 </form>
+
                 <div class="row">
-                    <div class="col-sm-12 d-flex justify-content-end gap-2">
-                        <button type="submit" class="btn btn-success">Submit</button>
-                        <a href="{{ route('kafa.manageActivity') }}" class="btn btn-danger">Cancel</a>
-                    </div>
+                    <form method="GET" action="{{ route('kafa.manageActivity') }}">
+                        <div class="col-sm-10 offset-sm-2">
+                            <button type="submit" class="btn btn-danger">Back</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
