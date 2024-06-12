5e39e353592e2f0ff005be5677d721767f970f1c
rizalromzi01 <rizaldanial0601@gmail.com>
2024-06-13 00:39:22 +0800

    1

--- resources/views/ManageKAFAActivities/KAFAEditActivity.blade.php
+++ resources/views/ManageKAFAActivities/KAFAEditActivity.blade.php
@@ -7,21 +7,25 @@
                 <h5 class="mb-0">{{$activity->activityName}}</h5>
             </div>
             <div class="card-body">
+                  <!-- Form to Update Activity -->
                 <form action="{{ route('kafa.updateActivity', $activity->id) }}" method="POST">
                     @csrf
                     @method('PUT')
+                      <!-- Activity Name -->
                     <div class="row mb-3">
                         <label for="activityName" class="col-sm-2 col-form-label text-center">Activity Name</label>
                         <div class="col-sm-10">
                             <input type="text" name="activityName" class="form-control" id="activityName" value="{{$activity->activityName}}">
                         </div>
                     </div>
+                        <!-- Venue -->
                     <div class="row mb-3">
                         <label for="venue" class="col-sm-2 col-form-label text-center">Venue</label>
                         <div class="col-sm-10">
                             <input type="text" name="venue" class="form-control" id="venue" value="{{$activity->venue}}">
                         </div>
                     </div>
+                        <!-- Date and Time -->
                     <div class="row mb-3">
                         <label for="dateStart" class="col-sm-2 col-form-label text-center">Date</label>
                         <div class="col-sm-4">
@@ -36,30 +40,35 @@
                             <input type="time" name="timeEnd" class="form-control" id="timeEnd" value="{{$activity->timeEnd}}">
                         </div>
                     </div>
+                        <!-- Attendees -->
                     <div class="row mb-3">
                         <label for="attendees" class="col-sm-2 col-form-label text-center">Attendees</label>
                         <div class="col-sm-10">
                             <input type="text" name="attendees" class="form-control" id="attendees" value="{{$activity->attendees}}">
                         </div>
                     </div>
+                        <!-- People In Charge -->
                     <div class="row mb-3">
                         <label for="organizerName" class="col-sm-2 col-form-label text-center">People In Charge</label>
                         <div class="col-sm-10">
                             <input type="text" name="organizerName" class="form-control" id="organizerName" value="{{$activity->organizerName}}">
                         </div>
                     </div>
+                        <!-- Description -->
                     <div class="row mb-3">
                         <label for="description" class="col-sm-2 col-form-label text-center">Description</label>
                         <div class="col-sm-10">
                             <textarea name="description" class="form-control" id="description" rows="3">{{$activity->description}}</textarea>
                         </div>
                     </div>
+                        <!-- Feedback -->
                     <div class="row mb-3">
                         <label for="feedback" class="col-sm-2 col-form-label text-center">Feedback</label>
                         <div class="col-sm-10">
                             <textarea name="feedback" class="form-control" id="feedback" rows="3">{{$activity->feedback}}</textarea>
                         </div>
                     </div>
+                        <!-- Submit Button -->
                     <div class="row">
                         <div class="col-sm-12 d-flex justify-content-end">
                             <button type="submit" class="btn btn-primary mx-2">Submit</button>
--- resources/views/ManageKAFAActivities/teacherActivity.blade.php
+++ resources/views/ManageKAFAActivities/teacherActivity.blade.php
@@ -3,12 +3,16 @@
 
 <div class="d-flex justify-content-end mb-3">
     <form action="{{ route('teacher.manageActivity') }}" method="GET" class="d-flex">
+         <!-- Search Input -->
         <input type="text" name="search" class="form-control" placeholder="Search" aria-label="Search" value="{{ request('search') }}" />
+         <!-- Search Button -->
         <button type="submit" class="btn btn-primary text-white btn-sm mx-3">Search</button>
     </form>
 </div>
 
+<!-- Activities Table -->
 <table class="table table-success rounded-4 w-100">
+     <!-- Table Header -->
     <thead class="table-secondary">
         <tr>
             <th scope="col" class="text-start px-5">Activity</th>
@@ -16,11 +20,15 @@
         </tr>
     </thead>
     <tbody>
+         <!-- Loop through activities -->
         @forelse ($activities as $activity)
             <tr>
+                 <!-- Activity Name Column -->
                 <td class="px-5">{{ $activity->activityName }}</td>
+                 <!-- Action Column -->
                 <td class="text-center">
                     <div class="btn-group" role="group">
+                         <!-- View Activity Button -->
                         <a href="{{ route('teacher.viewActivity', $activity->id) }}" class="btn btn-info btn-sm">
                             <i class="fas fa-eye" title="View"></i> View
                         </a>
